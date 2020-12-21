0185569ae0f04c96f1731aaf4eabfe7d2dedd4b0
Erik Stoel <e.stoel@portbase.com>
2020-12-21 17:24:51 +0100

    Update dependencies for backend_services and updated to net5.0

--- v1/backend_users/backend_users/Startup.cs
+++ v1/backend_users/backend_users/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace backend_users
 {
@@ -40,11 +40,12 @@ namespace backend_users
             services.AddScoped<IUsersLogic, UsersLogic>();
             
             services.AddMvc();
-            
+            services.AddControllers();
+
             // Register the Swagger generator, defining one or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "User Backend Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "User Backend Api", Version = "v1" });
             });
         }
 
@@ -66,7 +67,11 @@ namespace backend_users
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Backend Api");
             });
 
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
